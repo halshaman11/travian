efe53733e5d56fa426caca8d6a883b1fa8a5da3c
aeronz <w.chawan@gmail.com>
2013-11-11 22:44:08 +0700

    comment bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,66 +4,66 @@ import tm.game.*
 class BootStrap {
 
     def init = { servletContext ->
-    	def gaul = Race.findByName("Gauls")
-    	if(!gaul){
-    		gaul = new Race()
-    		gaul.name = "Gauls"
-    		gaul.save()
-    	}
-    	def roman = Race.findByName("Roman")
-    	if(!roman){
-    		roman = new Race()
-    		roman.name = "Roman"
-    		roman.save()
-    	}
-    	def teuton = Race.findByName("Teuton")
-    	if(!teuton){
-    		teuton = new Race()
-    		teuton.name = "Teuton"
-    		teuton.save()
-    	}
+    	// def gaul = Race.findByName("Gauls")
+    	// if(!gaul){
+    	// 	gaul = new Race()
+    	// 	gaul.name = "Gauls"
+    	// 	gaul.save()
+    	// }
+    	// def roman = Race.findByName("Roman")
+    	// if(!roman){
+    	// 	roman = new Race()
+    	// 	roman.name = "Roman"
+    	// 	roman.save()
+    	// }
+    	// def teuton = Race.findByName("Teuton")
+    	// if(!teuton){
+    	// 	teuton = new Race()
+    	// 	teuton.name = "Teuton"
+    	// 	teuton.save()
+    	// }
 
-    	def iku = Guild.findByName("อิคึ")
-    	if(!iku){
-    		iku = new Guild()
-    		iku.name = "อิคึ"
-    		iku.save()
-    	}
+    	// def iku = Guild.findByName("อิคึ")
+    	// if(!iku){
+    	// 	iku = new Guild()
+    	// 	iku.name = "อิคึ"
+    	// 	iku.save()
+    	// }
 
-    	iku = Guild.findByName("อิไตโย")
-    	if(!iku){
-    		iku = new Guild()
-    		iku.name = "อิไตโย"
-    		iku.save()
-    	}
+    	// iku = Guild.findByName("อิไตโย")
+    	// if(!iku){
+    	// 	iku = new Guild()
+    	// 	iku.name = "อิไตโย"
+    	// 	iku.save()
+    	// }
 
 
-        def user = User.findByUsername("admin")
-        if(!user){
-            def role = Role.findByAuthority("ROLE_ADMIN")
-            if(!role){
-                role = new Role()
-                role.authority = "ROLE_ADMIN"
-                role.save(flush:true)
-            }
-            user = new User()
-            user.username = "admin"
-            user.password = "qwer1234"
-            user.enabled = true
-            user.accountExpired = false
-            user.accountLocked = false
-            user.passwordExpired = false
-            user.validate()
-            println user.errors
-            user.save(flush:true)
-            UserRole.create user,role,true
-        }
-        def role = Role.findByAuthority("ROLE_USER")
-            if(!role){
-                role = new Role()
-                role.authority = "ROLE_USER"
-                role.save(flush:true)
-            }
+     //    def user = User.findByUsername("admin")
+     //    if(!user){
+     //        def role = Role.findByAuthority("ROLE_ADMIN")
+     //        if(!role){
+     //            role = new Role()
+     //            role.authority = "ROLE_ADMIN"
+     //            role.save(flush:true)
+     //        }
+     //        user = new User()
+     //        user.username = "admin"
+     //        user.password = "qwer1234"
+     //        user.enabled = true
+     //        user.accountExpired = false
+     //        user.accountLocked = false
+     //        user.passwordExpired = false
+     //        user.validate()
+     //        println user.errors
+     //        user.save(flush:true)
+     //        UserRole.create user,role,true
+     //    }
+     //    def role = Role.findByAuthority("ROLE_USER")
+     //        if(!role){
+     //            role = new Role()
+     //            role.authority = "ROLE_USER"
+     //            role.save(flush:true)
+     //        }
         /*user = User.findByUsername("aaa")
         if(!user){
             def role = Role.findByAuthority("ROLE_USER")
