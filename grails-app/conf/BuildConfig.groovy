26a8570726d7391c4d62d6e633c4b47cc842eea0
aeronz <w.chawan@gmail.com>
2013-11-11 16:54:42 +0700

    update plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -56,7 +56,7 @@ grails.project.dependency.resolution = {
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.5"
-
+        runtime ':console:1.2'
         build ":tomcat:$grailsVersion"
 
         runtime ":database-migration:1.3.2"
